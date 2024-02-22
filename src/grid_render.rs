93e7edb65cb719828471be7fe047690c67d5a87a
tobi <Tobias20.3@hotmail.de>
2024-02-22 19:01:43 +0100

    Cleanup grid_renderer

--- src/grid_render.rs
+++ src/grid_render.rs
@@ -1,12 +1,7 @@
 use std::fmt::Formatter;
-use std::fmt::{self, Display};
-use std::ops::Mul;
 
 use macroquad::prelude::*;
-use ndarray::{prelude, Array, Array2};
-
-const SHIFT_FACTOR: f32 = 250.0;
-const ZOOM_FACTOR: f32 = 1.1;
+use ndarray::Array2;
 
 #[derive(Debug, Clone)]
 pub enum BlockType {
@@ -14,31 +9,8 @@ pub enum BlockType {
     Filled,
 }
 
-#[derive(Debug)]
-pub struct Vec2D {
-    pub x: usize,
-    pub y: usize,
-}
-
-impl fmt::Display for BlockType {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        match *self {
-            BlockType::Empty => write!(f, "E"),
-            BlockType::Filled => write!(f, "F"),
-        }
-    }
-}
-
-impl Vec2D {
-    pub fn random_pos(level_size: usize) -> Vec2D {
-        let x = rand::gen_range(0, level_size);
-        let y = rand::gen_range(0, level_size);
-        Vec2D { x, y }
-    }
-}
-
 pub fn handle_mouse_inputs(display_factor: &mut f32, display_shift: &mut Vec2) {
-    let mouse_wheel_y = mouse_wheel().1;
+    /* let mouse_wheel_y = mouse_wheel().1;
 
     if mouse_wheel_y > 0.0 {
         *display_factor *= ZOOM_FACTOR;
@@ -49,7 +21,7 @@ pub fn handle_mouse_inputs(display_factor: &mut f32, display_shift: &mut Vec2) {
     let mouse_delta = mouse_delta_position();
     if is_mouse_button_down(MouseButton::Left) {
         *display_shift -= mouse_delta.mul(SHIFT_FACTOR);
-    }
+    } */
 }
 
 pub fn draw_grid_blocks(grid: &Array2<BlockType>, display_factor: f32, display_shift: Vec2) {
