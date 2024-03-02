d4d39e241eaf905ce7c753d113ba23820c4b311e
tobi <Tobias20.3@hotmail.de>
2024-03-02 18:59:43 +0100

    Add camera support for dynamic resizing

--- src/bin/camera_test.rs
+++ src/bin/camera_test.rs
@@ -20,7 +20,7 @@ fn get_display_factor() -> f32 {
     f32::min(screen_width() / MAP_WIDTH, screen_height() / MAP_HEIGHT)
 }
 
-fn reset_cam() -> Camera2D {
+fn set_cam(zoom: f32, x_offset: f32) -> Camera2D {
     dbg!(ASPECT_RATIO);
     let display_factor = get_display_factor();
     let x_view = display_factor * MAP_WIDTH;
@@ -34,6 +34,10 @@ fn reset_cam() -> Camera2D {
 
     cam.viewport = Some((0, y_shift as i32, x_view as i32, y_view as i32));
 
+    cam.offset.x = x_offset;
+    cam.zoom.x *= zoom;
+    cam.zoom.y *= zoom;
+
     set_camera(&cam);
 
     cam
@@ -41,42 +45,31 @@ fn reset_cam() -> Camera2D {
 
 #[macroquad::main("Camera")]
 async fn main() {
-    let mut cam = reset_cam();
+    let mut zoom = 1.0;
+    let mut x_offset = 0.0;
 
     loop {
-        set_camera(&cam);
-
         if is_key_released(KeyCode::W) {
-            cam.zoom.x *= ZOOM_FACTOR / ASPECT_RATIO;
-            cam.zoom.y *= ZOOM_FACTOR;
-            set_camera(&cam);
+            zoom *= ZOOM_FACTOR;
         } else if is_key_released(KeyCode::S) {
-            cam.zoom.x /= ZOOM_FACTOR / ASPECT_RATIO;
-            cam.zoom.y /= ZOOM_FACTOR;
-            set_camera(&cam);
+            zoom /= ZOOM_FACTOR;
         } else if is_key_released(KeyCode::R) {
-            cam = reset_cam(); // override
+            zoom = 1.0;
+            x_offset = 0.0;
         } else if is_key_released(KeyCode::D) {
-            cam.offset.x += SHIFT_FACTOR;
+            x_offset += SHIFT_FACTOR;
         } else if is_key_released(KeyCode::A) {
-            cam.offset.x -= SHIFT_FACTOR;
+            x_offset -= SHIFT_FACTOR;
         }
+        let cam = set_cam(zoom, x_offset);
 
         clear_background(LIGHTGRAY);
-        // lets assume drawing is from 0 - 100
+
         draw_line(0.0, 0.0, MAP_WIDTH, MAP_HEIGHT, 1.0, BLUE);
         draw_circle(0.0, 0.0, 5.0, RED);
-        draw_circle(cam.target.x, cam.target.y, 5.0, RED);
+        draw_circle(MAP_WIDTH / 2., MAP_HEIGHT / 2., 5.0, RED);
         draw_circle(MAP_WIDTH, MAP_HEIGHT, 5.0, YELLOW);
-        draw_rectangle_lines(0.0, 0.0, MAP_WIDTH, MAP_HEIGHT, 1.0, BLACK);
-
-        // draw_rectangle_lines(-1.0, -1.0, 2.0, 2.0, 0.01, RED);
-
-        // Back to screen space, render some text
-
-        // set_camera(&amera2D::default());
-
-        // set_default_camera();
+        draw_rectangle_lines(0.0, 0.0, MAP_WIDTH, MAP_HEIGHT, 5.0, BLACK);
 
         set_camera(&Camera2D::from_display_rect(Rect::new(
             0.0,
