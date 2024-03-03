8d3c2999368208674e25e2708f737857267b4177
tobi <Tobias20.3@hotmail.de>
2024-03-03 11:20:47 +0100

    Fix camera coordinates

--- src/bin/camera_test.rs
+++ src/bin/camera_test.rs
@@ -1,5 +1,6 @@
+use macroquad::experimental::camera::mouse;
 use macroquad::math::Rect;
-use macroquad::prelude::*;
+use macroquad::{camera, prelude::*};
 
 // TODO: this approach works rather well, but is has one major downside:
 // the viewport has the same aspect ratio as the map which allows
@@ -27,11 +28,12 @@ impl Editor {
 
     /// this should result in the exact same behaviour as if not using a camera at all
     fn reset_camera() {
+        // no idea why i dont have to use negative values here???
         set_camera(&Camera2D::from_display_rect(Rect::new(
             0.0,
-            screen_height(),
+            0.0,
             screen_width(),
-            -screen_height(),
+            screen_height(),
         )));
     }
 
@@ -39,11 +41,10 @@ impl Editor {
         let display_factor = Editor::get_display_factor();
         let x_view = display_factor * MAP_WIDTH;
         let y_view = display_factor * MAP_HEIGHT;
-
         let y_shift = screen_height() - y_view;
-        let mut cam =
-            Camera2D::from_display_rect(Rect::new(0.0, MAP_HEIGHT, MAP_WIDTH, -MAP_HEIGHT));
+        let mut cam = Camera2D::from_display_rect(Rect::new(0.0, 0.0, MAP_WIDTH, MAP_HEIGHT));
 
+        // so i guess this is (x, y, width, height) not two positions?
         cam.viewport = Some((0, y_shift as i32, x_view as i32, y_view as i32));
 
         cam.target.x -= self.offset.x * (MAP_WIDTH);
@@ -103,6 +104,11 @@ async fn main() {
         // draw target
         draw_circle(cam.target.x, cam.target.y, 2.5, ORANGE);
 
+        let mouse_pos_abs = mouse_position();
+        let mouse_viewport = cam.screen_to_world(mouse_pos_abs.into());
+
+        draw_circle_lines(mouse_viewport.x, mouse_viewport.y, 8.0, 2.0, GRAY);
+
         Editor::reset_camera();
 
         // draw globally
@@ -110,14 +116,17 @@ async fn main() {
         draw_circle(screen_width(), screen_height(), 15.0, ORANGE);
         draw_line(0.0, 0.0, screen_width(), screen_height(), 1.0, BLUE);
         draw_rectangle_lines(
-            cam.viewport().unwrap().0 as f32,
-            cam.viewport().unwrap().1 as f32,
+            0.0,
+            0.0,
             cam.viewport().unwrap().2 as f32,
             cam.viewport().unwrap().3 as f32,
             5.0,
             BLACK,
         );
 
+        draw_circle_lines(mouse_pos_abs.0, mouse_pos_abs.1, 10.0, 2.0, GRAY);
+
+        dbg!(mouse_pos_abs);
         next_frame().await
     }
 }
