4e10acdd90b15b0841b81ed3f015efc897740c19
tobi <Tobias20.3@hotmail.de>
2024-05-16 17:27:32 +0200

    Restructure ddnet bridge

--- src/bin/ddnet_bridge.rs
+++ src/bin/ddnet_bridge.rs
@@ -44,6 +44,177 @@ struct BridgeArgs {
     maps: PathBuf,
 }
 
+/// keeps track of the server bridge state
+struct ServerBridge {
+    /// econ connection to game server
+    econ: Econ,
+
+    /// stores information about vote while its still pending
+    pending_vote: Option<Vote>,
+
+    /// stores all available map generation configs
+    gen_configs: HashMap<String, GenerationConfig>,
+
+    /// stores start arguments
+    args: BridgeArgs,
+}
+
+impl ServerBridge {
+    fn new(args: BridgeArgs) -> ServerBridge {
+        ServerBridge {
+            econ: Econ::new(args.econ_port, args.telnet_buffer),
+            pending_vote: None,
+            gen_configs: GenerationConfig::get_all_configs(),
+            args,
+        }
+    }
+
+    fn start(&mut self) {
+        loop {
+            if let Some(data) = self.econ.read() {
+                if self.args.debug {
+                    println!("[RECV DEBUG]: {:?}", data);
+                }
+
+                if !self.econ.authed {
+                    self.check_auth(&data);
+                } else {
+                    self.check_vote(&data);
+                }
+            }
+        }
+    }
+
+    /// checks whether the econ message regards votes, if yes return a Vote struct
+    pub fn check_vote(&mut self, data: &String) {
+        // this regex detects all possible chat messages involving votes
+        let vote_regex = Regex::new(r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) I chat: \*\*\* (Vote passed|Vote failed|'(.+?)' called .+ option '(.+?)' \((.+?)\))\n").unwrap();
+        let result = vote_regex.captures_iter(&data);
+
+        for mat in result {
+            let _date = mat.get(1).unwrap();
+            let message = mat.get(2);
+
+            // determine vote event type
+            if let Some(message) = message.map(|v| v.as_str()) {
+                match message {
+                    "Vote passed" => {
+                        println!("[VOTE]: Success");
+                        self.handle_vote();
+                    }
+                    "Vote failed" => {
+                        self.pending_vote = None;
+                        println!("[VOTE]: Failed");
+                    }
+                    // vote started messages begin with 'player_name'
+                    _ if message.starts_with('\'') => {
+                        let player_name = mat.get(3).unwrap().as_str().to_string();
+                        let vote_name = mat.get(4).unwrap().as_str().to_string();
+                        let vote_reason = mat.get(5).unwrap().as_str().to_string();
+
+                        println!(
+                            "[VOTE]: vote_name={}, vote_reason={}, player={}",
+                            &vote_name, &vote_reason, &player_name
+                        );
+
+                        self.pending_vote = Some(Vote {
+                            _player_name: player_name,
+                            vote_name,
+                            vote_reason,
+                        });
+                    }
+                    // panic if for some holy reason something else matched the regex
+                    _ => panic!(),
+                }
+            }
+        }
+    }
+
+    /// checks whether the econ message regards authentication
+    pub fn check_auth(&mut self, data: &String) {
+        if data == "Enter password:\n" {
+            self.econ.send_rcon_cmd(self.args.econ_pass.clone());
+            println!("[AUTH] Sending login");
+        } else if data.starts_with("Authentication successful") {
+            println!("[AUTH] Success");
+            println!("[GEN] Generating initial map");
+            self.econ.authed = true;
+            self.generate_and_change_map(
+                &Seed::from_u64(1337),
+                &GenerationConfig::get_initial_config(),
+                &MapConfig::get_initial_config(),
+            );
+        } else if data.starts_with("Wrong password") {
+            println!("[AUTH] Wrong Password!");
+            std::process::exit(1);
+        }
+    }
+
+    pub fn handle_vote(&mut self) {
+        let vote = std::mem::replace(&mut self.pending_vote, None).expect("no pending vote");
+
+        if vote.vote_name.starts_with("generate") {
+            // derive Seed from vote reason
+            let seed = if vote.vote_reason == "No reason given" {
+                Seed::random()
+            } else if let Ok(seed_u64) = vote.vote_reason.parse::<u64>() {
+                Seed::from_u64(seed_u64)
+            } else {
+                Seed::from_string(&vote.vote_reason)
+            };
+
+            // split vote name to get selected preset
+            let mut vote_parts = vote.vote_name.split_whitespace();
+            let vote_type = vote_parts.next().expect("should have exactly two parts");
+            assert_eq!(vote_type, "generate");
+            let vote_preset = vote_parts.next().expect("should have exactly two parts");
+            assert!(vote_parts.next().is_none(), "should have exactly two parts");
+
+            // get config based on preset name
+            let gen_config = self
+                .gen_configs
+                .get(vote_preset)
+                .expect("preset does not exist!")
+                .clone();
+
+            self.generate_and_change_map(&seed, &gen_config, &MapConfig::get_initial_config());
+        }
+    }
+
+    fn generate_and_change_map(
+        &mut self,
+        seed: &Seed,
+        gen_config: &GenerationConfig,
+        map_config: &MapConfig,
+    ) {
+        println!("[GEN] Starting Map Generation!");
+        self.econ
+            .send_rcon_cmd(format!("say [GEN] Generating Map, seed={:?}", &seed));
+        let map_path = self
+            .args
+            .maps
+            .canonicalize()
+            .unwrap()
+            .join("random_map.map");
+        match Generator::generate_map(30_000, seed, gen_config, map_config) {
+            Ok(map) => {
+                println!("[GEN] Finished Map Generation!");
+                map.export(&map_path);
+                println!("[GEN] Map was exported");
+                self.econ.send_rcon_cmd("change_map random_map".to_string());
+                self.econ.send_rcon_cmd("reload".to_string());
+                self.econ.send_rcon_cmd("say [GEN] Done...".to_string());
+            }
+            Err(err) => {
+                println!("[GEN] Generation Error: {:?}", err);
+                self.econ
+                    .send_rcon_cmd(format!("say [GEN] Failed due to: {:}", err));
+                self.econ.send_rcon_cmd("say just try again :)".to_string());
+            }
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Vote {
     _player_name: String,
@@ -53,6 +224,7 @@ struct Vote {
 
 struct Econ {
     telnet: Telnet,
+    authed: bool,
 }
 
 impl Econ {
@@ -68,6 +240,7 @@ impl Econ {
                     println!("Coulnt establish telnet connection\nError: {:?}", err);
                     exit(1);
                 }),
+            authed: false,
         }
     }
 
@@ -87,145 +260,6 @@ impl Econ {
             .write(command.as_bytes())
             .expect("telnet write error");
     }
-
-    pub fn handle_vote(
-        &mut self,
-        vote: &Vote,
-        args: &BridgeArgs,
-        configs: &HashMap<String, GenerationConfig>,
-    ) {
-        if vote.vote_name.starts_with("generate") {
-            let seed = if vote.vote_reason == "No reason given" {
-                Seed::random()
-            } else if let Ok(seed_u64) = vote.vote_reason.parse::<u64>() {
-                Seed::from_u64(seed_u64)
-            } else {
-                Seed::from_string(&vote.vote_reason)
-            };
-
-            // split selected preset
-            let mut vote_parts = vote.vote_name.split_whitespace();
-            let vote_type = vote_parts.next().expect("should have exactly two parts");
-            assert_eq!(vote_type, "generate");
-            let vote_preset = vote_parts.next().expect("should have exactly two parts");
-            assert!(vote_parts.next().is_none(), "should have exactly two parts");
-
-            // get config based on preset name
-            let gen_config = configs.get(vote_preset).expect("preset does not exist!");
-
-            // TODO: add functionality to vote MapConfigs
-            generate_and_change_map(
-                args,
-                &seed,
-                gen_config,
-                &MapConfig::get_initial_config(),
-                self,
-            );
-        }
-    }
-}
-
-fn generate_and_change_map(
-    args: &BridgeArgs,
-    seed: &Seed,
-    gen_config: &GenerationConfig,
-    map_config: &MapConfig,
-    econ: &mut Econ,
-) {
-    println!("[GEN] Starting Map Generation!");
-    econ.send_rcon_cmd(format!("say [GEN] Generating Map, seed={:?}", &seed));
-    let map_path = args.maps.canonicalize().unwrap().join("random_map.map");
-    match Generator::generate_map(30_000, seed, gen_config, map_config) {
-        Ok(map) => {
-            println!("[GEN] Finished Map Generation!");
-            map.export(&map_path);
-            println!("[GEN] Map was exported");
-            econ.send_rcon_cmd("change_map random_map".to_string());
-            econ.send_rcon_cmd("reload".to_string());
-            econ.send_rcon_cmd("say [GEN] Done...".to_string());
-        }
-        Err(err) => {
-            println!("[GEN] Generation Error: {:?}", err);
-            econ.send_rcon_cmd(format!("say [GEN] Failed due to: {:}", err));
-            econ.send_rcon_cmd("say just try again :)".to_string());
-        }
-    }
-}
-
-fn start_bridge(args: &BridgeArgs) {
-    // this regex detects all possible chat messages involving votes
-    let vote_regex = Regex::new(r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) I chat: \*\*\* (Vote passed|Vote failed|'(.+?)' called .+ option '(.+?)' \((.+?)\))\n").unwrap();
-    let mut econ = Econ::new(args.econ_port, args.telnet_buffer);
-    let mut pending_vote: Option<Vote> = None;
-    let configs = GenerationConfig::get_all_configs();
-    let mut auth = false;
-
-    loop {
-        if let Some(data) = econ.read() {
-            if args.debug {
-                println!("[RECV DEBUG]: {:?}", data);
-            }
-
-            if data == "Enter password:\n" {
-                econ.send_rcon_cmd(args.econ_pass.clone());
-                println!("[AUTH] Sending login");
-            } else if data.starts_with("Authentication successful") {
-                println!("[AUTH] Success");
-                println!("[GEN] Generating initial map");
-                auth = true;
-                generate_and_change_map(
-                    args,
-                    &Seed::from_u64(1337),
-                    &GenerationConfig::get_initial_config(),
-                    &MapConfig::get_initial_config(),
-                    &mut econ,
-                );
-            } else if data.starts_with("Wrong password") {
-                println!("[AUTH] Wrong Password!");
-                std::process::exit(1);
-            } else if auth {
-                let result = vote_regex.captures_iter(&data);
-
-                for mat in result {
-                    let _date = mat.get(1).unwrap();
-                    let message = mat.get(2);
-
-                    // determine vote event type
-                    if let Some(message) = message.map(|v| v.as_str()) {
-                        match message {
-                            "Vote passed" => {
-                                println!("[VOTE]: Success");
-                                econ.handle_vote(pending_vote.as_ref().unwrap(), args, &configs);
-                            }
-                            "Vote failed" => {
-                                pending_vote = None;
-                                println!("[VOTE]: Failed");
-                            }
-                            // vote started messages begin with 'player_name'
-                            _ if message.starts_with('\'') => {
-                                let player_name = mat.get(3).unwrap().as_str().to_string();
-                                let vote_name = mat.get(4).unwrap().as_str().to_string();
-                                let vote_reason = mat.get(5).unwrap().as_str().to_string();
-
-                                println!(
-                                    "[VOTE]: vote_name={}, vote_reason={}, player={}",
-                                    &vote_name, &vote_reason, &player_name
-                                );
-
-                                pending_vote = Some(Vote {
-                                    _player_name: player_name,
-                                    vote_name,
-                                    vote_reason,
-                                });
-                            }
-                            // panic if for some holy reason something else matched the regex
-                            _ => panic!(),
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
 
 fn list_presets() {
@@ -237,7 +271,10 @@ fn list_presets() {
 
 fn main() {
     match Command::parse() {
-        Command::StartBridge(bridge_args) => start_bridge(&bridge_args),
+        Command::StartBridge(bridge_args) => {
+            let mut bridge = ServerBridge::new(bridge_args);
+            bridge.start();
+        }
         Command::ListPresets => list_presets(),
     }
 }
