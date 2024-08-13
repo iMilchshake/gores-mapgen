8be5a40e4edaef5759b4bf148a67e5657d2c3d72
Tylerstocks1 <tylerstocks100@outlook.com>
2024-08-13 20:05:13 +0100

    better error messages

--- src/bin/map_test.rs
+++ src/bin/map_test.rs
@@ -14,10 +14,10 @@ fn main() {
     let init_map_configs: HashMap<String, MapConfig> = MapConfig::get_all_configs();
 
     let seed: u64 = random::<u64>();
-   
+   //Loop through the keys of the hashmap 
     for gkey  in init_gen_configs.keys(){
         for mkey in init_map_configs.keys(){
-
+            //We access the generate_map arguments now so the time from instant is only timing the generation function and not retreiving the data for the var also.
             let seed = Seed::from_u64(seed);
             let gen_cfg = init_gen_configs.get(gkey).unwrap();
             let map_cfg = init_map_configs.get(mkey).unwrap();
@@ -30,17 +30,12 @@ fn main() {
                     gen_cfg,
                     map_cfg
                 );
-                let elapsed = now.elapsed();
-                match gen_result {
-                    Ok(_) => println!("GEN {gkey} WITH {mkey} ELAPSED TIME: {elapsed:?\n}"),
-                    Err(e) => println!("{e}"),
+                let elapsed = now.elapsed(); //compare the time difference
+                match gen_result { //Handiling the Result<t,e>
+                    Ok(_) => println!("GEN {gkey} WITH {mkey} MAP GEN | ELAPSED TIME: {elapsed:?\n}"),
+                    Err(e) => println!("ERROR IN GENERATING MAP: {e} | GENERATION THAT FAILED: {gkey} with {mkey}"),
                 }
             });
         }
     }
 }
-
-//MAKE GENERATION MAP OVER EVERY  GEN CFG & MAP CFG | MARK: DONE 
-//RNG SEED | MARK: DONE
-//TIME GENERATION  | MARK: DONE 
-//PRINT TO UI | MARK: NOT STARTED
